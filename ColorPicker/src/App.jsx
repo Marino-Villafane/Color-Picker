b46d856b926ac49213a61901355134e4fe14a8d5
root <root@DESKTOP-L7V09QC>
2023-09-12 22:17:24 -0500

    added colors in app.jsx

--- ColorPicker/src/App.jsx
+++ ColorPicker/src/App.jsx
@@ -1,4 +1,12 @@
+//import {useState} from 'react'
 // Write your Color component here
+
+
+
+// Write your Color component here
+const Color = ({color}) => {
+  return <div className={color}></div>
+};
 
 const App = () => {
   return (
@@ -7,7 +15,11 @@ const App = () => {
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="red"/>
+        <Color color="blue"/>
+        <Color color="green"/>
+        </div>
     </div>
   );
 };
