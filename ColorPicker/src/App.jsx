63415e094bf2bb62527145a0176639c144b09c97
root <root@DESKTOP-L7V09QC>
2023-09-12 23:37:11 -0500

    delete App.css and update index.css

--- ColorPicker/src/App.jsx
+++ ColorPicker/src/App.jsx
@@ -1,12 +1,12 @@
-//import {useState} from 'react'
-// Write your Color component here
+import { useState } from 'react'
 
 
-
-// Write your Color component here
+// Define the Color component
 const Color = ({color}) => {
-  return <div className={color}></div>
+  return <div className={color}></div>;
 };
+
+
 
 const App = () => {
   return (
@@ -15,11 +15,13 @@ const App = () => {
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">
-        <Color color="red"/>
-        <Color color="blue"/>
-        <Color color="green"/>
-        </div>
+      <div id="colors-list">{
+      <>
+      <Color color="red" />
+      <Color color="blue" />
+      <Color color="green" />
+      </>
+      }</div>
     </div>
   );
 };
